5101f3c19b1a84ed405161fbf2d61e48afe046da
Esteban Morales <red_bandit_11@hotmail.com>
2017-11-17 12:35:05 -0400

    added  new imports

--- src/providers/amaranthus-db/amaranthus-db.ts
+++ src/providers/amaranthus-db/amaranthus-db.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import 'rxjs/add/operator/retry';
+// import 'rxjs/add/operator/retry';
+import { Observable } from 'rxjs/Observable';
+import { catchError, map, tap, retry } from 'rxjs/operators';
 /*
   Generated class for the AmaranthusDbProvider provider.
 
@@ -12,13 +14,19 @@ export class AmaranthusDBProvider {
 
   constructor(private http: HttpClient) {
     console.log('Hello AmaranthusDbProvider Provider');
+    this.createDB();
   }
+  
 getAllStudents(){
   return this.http.get('./assets/mock-students.json')
-  
+  .pipe(
+      catchError(this.handleError('getHeroes'))
+    );
 }
 
 handleError(error){
   console.error(error);
 }
+
+createDB(){}
 }
